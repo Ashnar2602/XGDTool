424d7b4eaa61cf69eb7ee980da8ab5290e8459cd
wiredopposite <jackxhorizon@gmail.com>
2026-05-29 16:42:46 -0600

    implement more avl tree methods

---
+++ Avl/Interator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XGDTool.Format;
+
+namespace XGDTool.Avl
+{
+    public class Interator
+    {
+        public class Entry
+        {
+            public readonly long Offset;
+            public readonly bool DirectoryEntry;
+            public readonly Node Node;
+
+            public Entry(long offset, bool directoryEntry, Node node)
+            {
+                Offset = offset;
+                DirectoryEntry = directoryEntry;
+                Node = node;
+            }
+        }
+
+        public List<Entry> Entries { get; private set; } = new();
+
+        public Interator(Tree tree)
+        {
+            var nodes = new List<Node>();
+
+            Tree.Traverse(
+                Traversal.Prefix, 
+                tree.RootNode.Subdirectory, 
+                0, 
+                CollectNodes, 
+                nodes);
+
+            foreach (var node in nodes)
+            {
+                if (node.Subdirectory == null || node.Subdirectory is not EmptyNode)
+                {
+                    Entries.Add(new Entry(node.StartSector * XISO.SECTOR_SIZE, false, node));
+                }
+                else
+                {
+                    Entries.Add(new Entry(node.DirectoryStart + node.DirectoryOffset, true, node));
+                }
+            }
+
+            Entries.Sort((a, b) => a.Offset.CompareTo(b.Offset));
+        }
+
+        private void CollectNodes(Node? node, int depth, List<Node>? nodes)
+        {
+            if (node == null || node is EmptyNode)
+                return;
+
+            nodes!.Add(node);
+
+            if (node.Subdirectory != null)
+            {
+                Tree.Traverse(Traversal.Prefix, node.Subdirectory, 0, CollectNodes, nodes);
+            }
+        }
+    }
+}
--- Image/Writer.cs
+++ Image/Writer.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XGDTool.Format;
 
 namespace XGDTool.Image
 {
-    internal class Writer
+    public class Writer
     {
+        public Writer(Reader reader)
+        {
+
+        }
+
+        protected XISO.DirectoryHeader CreateDirectoryHeader(Avl.Node node)
+        {
+            var header = new XISO.DirectoryHeader();
+            var subDirEmpy = node.Subdirectory is Avl.EmptyNode;
+
+            header.LeftOffset = 
+                (node.LeftChild != null) 
+                    ? (ushort)(node.LeftChild.DirectoryOffset / 4) 
+                    : (ushort)0;
+
+            header.RightOffset = 
+                (node.RightChild != null)
+                    ? (ushort)(node.RightChild.DirectoryOffset / 4) 
+                    : (ushort)0;
+
+            header.StartSector = (uint)node.StartSector;
+
+            if (node.Subdirectory != null || subDirEmpy)
+            {
+                header.FileSize =
+                    (uint)node.FileSize +
+                    (uint)((XISO.SECTOR_SIZE - (node.FileSize % XISO.SECTOR_SIZE)) % XISO.SECTOR_SIZE);
+            }
+            else
+            {
+                header.FileSize = (uint)node.FileSize;
+            }
+
+            header.Attributes = (node.Subdirectory != null || subDirEmpy) 
+                ? XISO.DirAttribute.Directory 
+                : XISO.DirAttribute.File;
+
+            header.NameLength = (byte)Math.Min(node.Filename.Length, 255);
+
+            return header;
+        }
     }
 }
