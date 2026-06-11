1699df0e5b257205ae20433f883a8eb13f7d85fe
wiredopposite <jackxhorizon@gmail.com>
2026-06-11 16:10:18 -0600

    implement descriptor to enum conversion method

--- XGDTool.Lib/Util/EnumExt.cs
+++ XGDTool.Lib/Util/EnumExt.cs
@@ -21,4 +21,23 @@ public static class EnumExt
 
         return attr?.Description ?? value.ToString();
     }
+
+    public static T FromDescription<T>(string description) where T : Enum
+    {
+        foreach (var field in typeof(T).GetFields())
+        {
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                if (attribute.Description == description)
+                    return (T)field.GetValue(null)!;
+            }
+            else
+            {
+                if (field.Name == description)
+                    return (T)field.GetValue(null)!;
+            }
+        }
+
+        throw new ArgumentException($"No enum value with description '{description}' found in {typeof(T)}.");
+    }
 }
